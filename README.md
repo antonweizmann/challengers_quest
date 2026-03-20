7ce6eeb9c1752809c310ed49c4b3c454c2e1b5a5
antonweizmann <anton.weizmann@ru.nl>
2026-03-20 22:35:20 +0100

    started some work on game logic, setup log file with loggin module

--- README.md
+++ README.md
@@ -43,7 +43,39 @@ Dungeon Attributs:
 	Enemy number: int
 	Enemies : list of Enemy class
 	Player: Player class
-	
+
+
+
+Game Logic:
+Game Class - responsible for handling main game loop
+	tasks include starting game environment, holding player, loading dungeon floors, output logging, death handling
+
+	Attributes:
+		dungeon: Dungeon Class
+		log_file: fd
+
+	Methods:
+		__init__: setup game environment,  open log_file in append.
+
+		game_loop: handles main game loop
+
+		game_start: welcome message to player, choose class, choose difficulty, initialize dungeon, start game_loop:
+
+		game_exit: says taunting goodbye message, closes log_file
+
+
+Dungeon class
+	tasks include holding enemies, determining difficulty, spawning shops and rest pools
+
+Shop class - holds 4 random items min 2 weapons
+	tasks include handeling buying items
+
+Inherited Dungeon Classes (Easy and Hard)
+
+
+
+Ideas to add ?
+Crafting - Lootdrops
 
 
 This project was done by Taj Jursa, Luuc Koojiman and Anton Weizmann
---
+++ game_logic/game.py
@@ -0,0 +1,42 @@
+import logging as lg
+import os
+
+class challengers_quest:
+	def _create_log_file(self):
+		if not os.path.exists("logs"):
+			os.mkdir("logs")
+		lg.basicConfig(filename="logs/game.log", level=lg.INFO, filemode="a", format="%(asctime)s - %(levelname)s - %(message)s")
+		self.log = lg.getLogger(self.__class__.__name__)
+
+	def __init__(self):
+		self._create_log_file()
+		self.log.info("Challenger Quest object created")
+
+	def game_start(self):
+		print("---- WELCOME TO CHALLENGERS QUEST ----")
+		print("")
+
+
+
+if __name__ == "__main__":
+	cq = challengers_quest()
+	cq.log.warning("Test2")
+
+
+
+# Game Class - responsible for handling main game loop
+# 	tasks include starting game environment, holding player, loading dungeon floors, output logging, death handling
+
+# 	Attributes:
+# 		dungeon: Dungeon Class
+# 		log_file: fd
+
+# 	Methods:
+# 		__init__: setup game environment,  open log_file in append.
+
+# 		game_loop: handles main game loop
+
+# 		game_start: welcome message to player, choose class, choose difficulty, initialize dungeon, start game_loop:
+
+# 		game_exit: says taunting goodbye message, closes log_file
+
