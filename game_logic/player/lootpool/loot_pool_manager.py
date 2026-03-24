c403666cf738b45a0455cf1989dbc428f6111264
Luuc Kooijman <luuc.kooijman@ru.nl>
2026-03-24 18:47:32 +0100

    made my changes

--- game_logic/player/lootpool/loot_pool_manager.py
+++ game_logic/player/lootpool/loot_pool_manager.py
@@ -1,5 +1,5 @@
 import random as rdm
-
+import os
 def gen_random_int(upper_limit: int):
     return rdm.randint(0, upper_limit)
 
@@ -7,32 +7,39 @@ def gen_random_int(upper_limit: int):
 weapon_pool.txt lines contain an unique item using # seperater in the following format:
 <name>#<rarity>#<damage>#<stamina_use>
 '''
-def get_random_weapon(in_file="weapon_pool.txt"):
+def get_random_weapon(filename="weapon_pool.txt"):
+    
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, filename)
+    
     try:
-        with open(in_file, "r") as file:
+        with open(file_path, "r") as file:
             lines = file.readlines() # Reads everything into a list memory once
             line_to_get = gen_random_int(len(lines) - 1)
             line = lines[line_to_get]
             name, rarity, damage, stamina_use = line.strip().split('#')
         return {'name': name, 'rarity': rarity, 'damage': int(damage), 'stamina_use': int(stamina_use)}
     except FileNotFoundError as e:
-        print(f"ERROR: Could not find {in_file}! Giving a default weapon instead.")
+        print(f"ERROR: Could not find {filename}! Giving a default weapon instead.")
         return {'name': 'Broken Stick', 'rarity': 'Poor', 'damage': 10, 'stamina_use': 1}
 
 '''
 comsumable_pool.txt lines contain an unique item using # seperater in the following format:
 <name>#<rarity>#<use_type>#<effect_amt>
 '''
-def get_random_consumable(in_file="consumable_pool.txt"):
+def get_random_consumable(filename="consumable_pool.txt"):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, filename)
+    
     try:
-        with open(in_file, "r") as file:
+        with open(file_path, "r") as file:
             lines = file.readlines() # Reads everything into a list memory once
             line_to_get = gen_random_int(len(lines) - 1)
             line = lines[line_to_get]
             name, rarity, use_type, effect_amt = line.strip().split('#')
         return {'name': name, 'rarity': rarity, 'use_type': use_type, 'effect_amt': int(effect_amt)}
     except FileNotFoundError as e:
-        print(f"ERROR: Could not find {in_file}! Giving a default consumable instead.")
+        print(f"ERROR: Could not find {filename}! Giving a default consumable instead.")
         return {'name': 'Mouldy Bread', 'rarity': 'Poor', 'use_type': 'health', 'effect_amt': 5}
 
 
